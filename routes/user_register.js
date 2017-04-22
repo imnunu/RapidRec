c492d5585c1b43ac2fcbc3fb5e783440d6b9acce
Bassim <bassimsadik@hotmail.com>
2017-04-22 19:11:20 +0000

    created register route

--- routes/user_register.js
+++ routes/user_register.js
@@ -5,7 +5,49 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 
 module.exports = (knex) => {
+
+  function findExistingEmail(email) {
+    return knex
+      .select('email')
+      .from('users')
+      .where({email: email})
+      .limit(1)
+      .then((results) => {
+        if (results.length){
+          return Promise.resolve(email);
+        } else {
+          return Promise.resolve();
+        }
+      })
+  }
+
+  // function createNewUser(firstName, lastName, email, password, )
+
   router.post("/", (req, res) => {
+
+    let date = new Date();
+    let existingEmail = findExistingEmail(req.body.email);
+
+    existingEmail.then((email) => {
+      if (email) {
+        res.send("Error: An account already exists with that email");
+        res.redirect('/');
+      } else {
+        knex.insert({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, 10),
+          created_at: date
+        })
+        .into('users')
+        .returning('id')
+        .then(function(id){
+          console.log(id)
+          req.session.user_id = id;
+        })
+      }
+    })
   });
   return router;
 }
