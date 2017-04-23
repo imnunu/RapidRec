4fa0110edffe43bcdd68fa63ec5c3db25ea0502c
Ellen Chang <imnunu@gmail.com>
2017-04-23 02:53:15 +0000

    merge index and moved css file

--- public/scripts/googleApi.js
+++ public/scripts/googleApi.js
@@ -63,3 +63,13 @@ function createMarker(place) {
     infowindow.open(map, this);
   });
 }
+
+
+
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map=new google.maps.Map(document.getElementById("map"),mapProp);
+  }
