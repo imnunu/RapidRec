0a9ec458d2ac6a0850f15474fc1d78fffbee3c6e
Bassim <bassimsadik@hotmail.com>
2017-04-26 03:13:36 +0000

    commiting to save

--- public/scripts/event.js
+++ public/scripts/event.js
@@ -10,7 +10,7 @@ function loadPage(id){
     info.forEach(function (item) {
       console.log(item);
       $('#intro .event_title').text(item.title);
-       $('#intro .description').text('Description: ' + item.description);
+      $('#intro .description').text('Description: ' + item.description);
       $('#intro .event_location').text('Location: ' + item.location);
       $('#intro .number_of_players').text('Players: ' + item.number_of_players);
       $('#intro .event_start_time').text('Start time: ' + moment(item.start_time).calendar());
@@ -20,4 +20,12 @@ function loadPage(id){
     //   $("<div>").text('Location: ' + i.location).appendTo($("#intro > event_title"));
 
   });
+}
+
+function loadGameMembers(){
+
+};
+
+function createMembersElement(){
+
 }
